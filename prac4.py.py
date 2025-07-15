d269fd6225eb6ef7cac3f5c20a506cf926455c19
Siddhi-D-07 <degaonkarsiddheshwari@gmail.com>
2025-07-15 11:43:23 +0530

    Add files via upload

---
+++ prac4.py.py
@@ -0,0 +1,28 @@
+print("control statements")
+print("if statement :")
+num=int(input("enter the num:"))
+
+if (num%2)==0:
+    print(num,"is even")
+
+print("if-else statement :")
+
+if (num%2)==0:
+    print(num,"is even")
+
+else:
+    print(num,"is odd")
+
+print("if-elif-else statement :")
+
+if(num > 0):
+    print(num,"is positive")
+
+elif (num < 0):
+    print(num,"is negative")
+
+else:
+    print(num,"is zero") 
+
+
+        
